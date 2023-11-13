46d8db97a73ff4eb7d6d478fe3baab6c6a1c1cff
Maurice <mauriceglenn2@gmail.com>
2023-11-13 00:28:05 -0600

    Nav almost complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import Nav from '../components/Nav';
-import './App.css';
+import Nav from './components/Nav.jsx';
 
 function App() {
   return (
---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav>
+      
+      <div className='logo__container'>
+        <img src="" alt="" />
+        <h1>Anime Finder</h1>
+      </div>
+      <div>
+        <ul className='links'>
+          <li className='link__list'>Log In</li>
+          <li className='link__list'>Sign up</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
