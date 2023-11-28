730deee0aa4d629d186b54f4706bd1b28ce07332
Maurice <mauriceglenn2@gmail.com>
2023-11-27 21:41:04 -0600

    working on moble view

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Nav() {
+  function openMenu(){
+    document.body.classList += ' menu__open'
+  }
+
+  function closeMenu(){
+    document.body.classList.remove('menu__open')
+  }
+
   return (
     <nav>
       
       <div className='logo__container'>
-        <img src="" alt="" />
         <Link to={`/`}><h1>Anime Explorer</h1></Link>
       </div>
       <div>
@@ -15,7 +23,32 @@ function Nav() {
           <a href=""><li className='link__list'>Blog</li></a>
           <li className='link__list login__btn'>Log In</li>
           <li className='link__list signup__btn'>Sign up</li>
+          <button className='btn__menu' onClick={openMenu}>
+            <FontAwesomeIcon icon="bars" />
+          </button>
         </ul>
+        <div className="menu__backdrop">
+          <button className='btn__menu btn__menu--close' onClick={closeMenu}>
+            <FontAwesomeIcon icon="times" />
+          </button>
+          <ul className='menu__links'>
+            <li className="menu__list">
+              <Link to={`/`}>Home</Link>
+            </li>
+            <li className="menu__list">
+              <Link to={`/`}>About</Link>
+            </li>
+            <li className="menu__list">
+              <Link to={`/`}>Log-In</Link>
+            </li>
+            <li className="menu__list">
+              <Link to={`/`}>Sign-up</Link>
+            </li>
+            
+            
+
+          </ul>
+        </div>
       </div>
     </nav>
   )
