797b91970335474eb8c2c42e79d69667bbe85090
Maurice <mauriceglenn2@gmail.com>
2023-12-12 12:08:33 -0600

    adjusted animation speed

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -11,13 +11,13 @@ function Landing() {
     }, [])
    
     return (
-        <section className='landing__contianer'>
+        <section className='landing__contianer' data-aos-easing="ease-in">
             <div className='gradient'></div>
             <LandingSlider />
             <div className='landing__text--wrapper'>
-                <h1 className='landing__title' data-aos="fade-up" >Anime Explorer</h1>
-                <p className='landing__para' data-aos="fade-up" >Explore thousands of anime titles.</p>
-                <ScrollLink data-aos="fade-up"  className="try__btn-link" to="search" smooth={"true"} duration={500}>
+                <h1 className='landing__title' data-aos="fade-up" data-aos-delay="1000" >Anime Explorer</h1>
+                <p className='landing__para' data-aos="fade-up" data-aos-delay="1500">Explore thousands of anime titles.</p>
+                <ScrollLink data-aos="fade-up" data-aos-delay="2000" className="try__btn-link" to="search" smooth={"true"} duration={500}>
                     <span className='try__btn' >Try Now</span>
                 </ScrollLink>
             </div>
